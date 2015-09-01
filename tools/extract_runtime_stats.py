63ef8b673b7b138f13a798d37c2a9e34092d54c3
colin-scott <cs@cs.berkeley.edu>
2015-08-31 19:10:00 -0700

    Extract runtime, replays

---
+++ tools/extract_runtime_stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import json
+import argparse
+
+def parse(logfile):
+  '''Input: logfile.'''
+  with open(logfile) as f:
+    return json.load(f)
+
+def sum(lst, extractor):
+  return reduce(lambda x,y: x + y, map(extractor, lst), 0)
+
+def sum_replays(lst):
+  return sum(lst, lambda o: o["total_replays"])
+
+def sum_runtime(lst):
+  return sum(lst, lambda o: o["prune_duration_seconds"])
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--start_index', type=int,
+    default=0,
+    help='Which index to start summing from')
+  parser.add_argument('-e', '--end_index', type=int,
+    default=-1,
+    help='Which index to end summing from')
+  parser.add_argument('path', metavar='path', type=str)
+
+  args = parser.parse_args()
+
+  lst = parse(args.path)
+  end_idx = len(lst) if (args.end_index == -1) else args.end_index
+  subseq = lst[args.start_index:end_idx]
+
+  print("Runtime: %0.2f, Replays: %d" % (sum_runtime(subseq), sum_replays(subseq)))
