4ddce62ca9bef799ef88ea9218e74b985b770c1b
colin-scott <cs@cs.berkeley.edu>
2015-09-02 15:30:02 -0700

    Add in paths for all case studies

--- tools/make_runtime_table.py
+++ tools/make_runtime_table.py
@@ -4,7 +4,13 @@ from extract_runtime_stats import *
 
 # long runs:
 experiments = {
+  "raft-45": "akka-raft-fuzz-long_2015_09_02_12_18_50_DDMin_STSSchedNoPeek",
+  "raft-46": "akka-raft-fuzz-long_2015_09_02_12_36_41_DDMin_STSSchedNoPeek",
+  "raft-56": "akka-raft-fuzz-long_2015_09_02_12_48_32_DDMin_STSSchedNoPeek",
+  "raft-58-init": "akka-raft-fuzz-long_2015_09_02_13_25_20_DDMin_STSSchedNoPeek",
+  "raft-58-log-match": "akka-raft-fuzz-long_2015_08_30_20_59_21_DDMin_STSSchedNoPeek",
   "raft-42": "akka-raft-fuzz-long_2015_08_30_21_57_21_DDMin_STSSchedNoPeek",
+  "raft-66": "akka-raft-fuzz-long_2015_08_12_23_55_04_DDMin_STSSchedNoPeek"
 }
 
 for k,v in experiments.iteritems():
