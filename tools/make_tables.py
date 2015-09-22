e4b8d691dfc3de0e6b8f7a2b36fa85300ca034fa
colin-scott <cs@cs.berkeley.edu>
2015-09-22 11:40:18 -0700

    Add first spark bug

--- tools/make_tables.py
+++ tools/make_tables.py
@@ -11,6 +11,7 @@ experiments = [
   ("raft-58-log-match", "akka-raft-fuzz-long_2015_08_30_20_59_21_DDMin_STSSchedNoPeek"),
   ("raft-42", "akka-raft-fuzz-long_2015_08_30_21_57_21_DDMin_STSSchedNoPeek"),
   ("raft-66", "akka-raft-fuzz-long_2015_08_12_23_55_04_DDMin_STSSchedNoPeek"),
+  ("spark-2294", "spark-fuzz_2015_09_22_10_35_51_DDMin_STSSchedNoPeek"),
   ("spark-2294-blocks", "spark-fuzz_2015_09_21_17_53_01_DDMin_STSSchedNoPeek")
 ]
 
