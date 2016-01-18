83ef3ca66b52953abfb2499c7cf10753774ac0aa
colin-scott <cs@cs.berkeley.edu>
2016-01-17 21:52:44 -0800

    Optimal trace for raft-66

--- tools/make_tables.py
+++ tools/make_tables.py
@@ -23,7 +23,7 @@ experiments = [
 #  ("raft-56-opt", "akka-raft-interactive_2016_01_09_18_05_42"), # 4 nodes
 #  ("raft-58-init-opt", "akka-raft-interactive_2016_01_12_18_42_05"), # 4 nodes, 3 sends
 #  ("raft-42", "akka-raft-interactive_2016_01_17_21_21_00"), # 4 nodes
-
+#  ("raft-66", "akka-raft-interactive_2016_01_17_21_51_48"), # 4 nodes
 
 NUM_NODES = 4
 
